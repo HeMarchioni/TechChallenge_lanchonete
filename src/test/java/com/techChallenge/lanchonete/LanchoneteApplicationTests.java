681225d2abb8afd07b2ffa904878a02ce6a307c4
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2023-06-26 01:06:14 -0300

    commit inicial

---
+++ src/test/java/com/techChallenge/lanchonete/LanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techChallenge.lanchonete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
