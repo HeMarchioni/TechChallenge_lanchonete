292f8c073a386449684531a8416e70e787acf9ae
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2023-07-09 23:12:58 -0300

    Finalização Readme.

--- README.md
+++ README.md
@@ -1,1 +1,165 @@
 # TechChallenge_lanchonete
+
+# Passos para execução do projeto
+
+1. Clone o repositório.
+2. Certifique-se de ter o Docker instalado na sua máquina.
+3. No diretório raiz do projeto, abra o terminal.
+4. Execute o seguinte comando para subir a aplicação:
+ ```shell
+   docker-compose up --build
+```
+
+Para desligar a aplicação
+
+```shell
+   docker-compose down
+```
+
+## Testes
+
+Depois que a Aplicação buildar e subir
+
+- Para consultar as rotas da API, acesse: [http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html).
+- Você pode usar o Postman ou o Swagger para testar as requisições.
+
+## Cadastro do Cliente
+
+Para cadastrar um cliente, faça uma requisição POST para a rota `http://localhost:8080/cliente/create`.
+
+### Exemplo de requisição:
+
+```http
+POST http://localhost:8080/cliente/create
+Content-Type: application/json
+
+{
+  "nomeCliente": "Nome do Cliente",
+  "email": "cliente@example.com",
+  "cpf": "12345678900"
+}
+```
+
+## Identificação do Cliente via CPF
+
+Para identificar um cliente com base no CPF, faça uma requisição POST para a rota `http://localhost:8080/cliente/busca/cpf/email`.
+
+### Exemplo de requisição:
+
+```http
+POST http://localhost:8080/cliente/busca/cpf/email
+Content-Type: application/json
+
+{
+  "cpf": "12345678900"
+}
+```
+
+## Criação, edição e remoção de produtos
+
+Para criar um produto, faça uma requisição POST para a rota `http://localhost:8080/produto/create`.
+
+"categoria" -> é um enum valores aceitos (LANCHE, ACOMPANHAMENTO, BEBIDA, SOBREMESA)
+
+### Exemplo de requisição:
+
+```http
+POST http://localhost:8080/produto/create
+Content-Type: application/json
+
+{
+  "nomeProduto": "Nome do Produto",
+  "categoria": "LANCHE",
+  "descricaoProduto": "Descrição do Produto",
+  "preco": 10.5,
+  "imagem": "url-da-imagem"
+}
+```
+
+Para editar um produto existente, faça uma requisição PUT para a rota `http://localhost:8080/produto`.
+
+### Exemplo de requisição:
+
+```http
+PUT http://localhost:8080/produto
+Content-Type: application/json
+
+{
+  "id": 1,
+  "nomeProduto": "Nome do Produto Atualizado",
+  "categoria": "ACOMPANHAMENTO",
+  "descricaoProduto": "Nova descrição do Produto",
+  "preco": 15.9,
+  "imagem": "nova-url-da-imagem"
+}
+```
+
+Para remover um produto, faça uma requisição DELETE para a rota `http://localhost:8080/produto/{id}`, onde `{id}` é o ID do produto a ser removido.
+
+**Regra:** Não é possível apagar um produto que está em uso em um pedido em aberto não finalizado.
+
+### Exemplo de requisição:
+
+```http
+DELETE http://localhost:8080/produto/1
+```
+
+Para buscar produtos por categoria, faça uma requisição GET para a rota `http://localhost:8080/produto/categoria/{categoria}`, onde `{categoria}` é a categoria desejada.
+
+### Exemplo de requisição:
+
+```http
+GET http://localhost:8080/produto/categoria/LANCHE
+```
+
+
+## Pedidos / Pagamento
+
+Obs.: O status do Pedido é um enum com valores aceitos (AGUARDANDO_PAGAMENTO, RECEBIDO, PREPARACAO, PRONTO, FINALIZADO)
+
+### Criar um Pedido
+
+Para criar um pedido, faça uma requisição POST para a rota `http://localhost:8080/pedido/create`.
+
+Obs.: O ID do cliente é opcional.
+
+Exemplo de requisição:
+
+```http
+POST http://localhost:8080/pedido/create
+Content-Type: application/json
+
+{
+  "cliente": 0,
+  "listaProduto": [0],
+  "observacao": "Observação do Pedido"
+}
+```
+**Fluxo de Pedido / Pagamento:**
+Assim que o pedido é criado, o status do Pedido é definido como <b>AGUARDANDO_PAGAMENTO</b>.<br>Em seguida, é feita uma requisição para uma API de pagamento (simulação), que retorna um `qr_data` com o código para gerar um QR code.
+
+Após o pagamento ser realizado, a notificação é recebida na rota `http://localhost:8080/notificacoes/pagamento`, onde o status do pedido é atualizado para RECEBIDO e entra na fila.
+
+**Regra:** Os 5 primeiros pedidos com status <b>RECEBIDO</b> são alterados para <b>PREPARACAO</b>.
+
+Quando um pedido é <b>FINALIZADO</b>, o próximo pedido na fila com status <b>RECEBIDO</b> é movido para <b>PREPARACAO</b>.
+
+### Pedido Pronto
+
+Rota para alterar o status do pedido para PRONTO.
+
+Método: PUT
+Rota: http://localhost:8080/pedido/pronto/{id} (passar o ID do pedido na rota)
+
+### Pedido Finalizado
+
+Rota para alterar o status do pedido para FINALIZADO.
+
+Método: PUT
+Rota: http://localhost:8080/pedido/finalizado/{id} (passar o ID do pedido na rota)
+
+### Listar os pedidos
+
+Método: GET
+Rota: http://localhost:8080/pedido
+```
--- src/main/java/com/techChallenge/lanchonete/Populate.java
+++ src/main/java/com/techChallenge/lanchonete/Populate.java
@@ -3,8 +3,11 @@ package com.techChallenge.lanchonete;
 
 import com.techChallenge.lanchonete.core.applications.Enum.CategoriaProduto;
 import com.techChallenge.lanchonete.core.applications.dtos.in.ClienteDTO;
+import com.techChallenge.lanchonete.core.applications.dtos.in.PedidoDTO;
 import com.techChallenge.lanchonete.core.applications.dtos.in.ProdutoDTO;
+import com.techChallenge.lanchonete.core.applications.dtos.out.PedidoOutDTO;
 import com.techChallenge.lanchonete.core.applications.ports.interfaces.ClienteServicePort;
+import com.techChallenge.lanchonete.core.applications.ports.interfaces.PedidoServicePort;
 import com.techChallenge.lanchonete.core.applications.ports.interfaces.ProdutoServicePort;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +15,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+
+//Classe usada para teste para Popular dados se o banco estiver vazio
 @Component
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class Populate implements CommandLineRunner {
 
     private final ProdutoServicePort produtoRepository;
     private final ClienteServicePort clienteRepository;
+    private final PedidoServicePort pedidoServicePort;
 
-    public void insertProdutos() {
+    public boolean insertProdutos() {
         try {
 
             ArrayList<ProdutoDTO> produtos = new ArrayList<>();
@@ -34,6 +41,7 @@ public class Populate implements CommandLineRunner {
             produto.setImagen("Hamburguer Simples");
             produtos.add(produto);
 
+            produto = new ProdutoDTO();
             produto.setNomeProduto("X-Bacon");
             produto.setDescricaoProduto("Pão de brioche, carne de 110g, mussarela e bacon");
             produto.setCategoria(CategoriaProduto.LANCHE);
@@ -41,6 +49,7 @@ public class Populate implements CommandLineRunner {
             produto.setImagen("X-Bacon");
             produtos.add(produto);
 
+            produto = new ProdutoDTO();
             produto.setNomeProduto("Fritas");
             produto.setDescricaoProduto("Batata palito frita");
             produto.setCategoria(CategoriaProduto.ACOMPANHAMENTO);
@@ -48,6 +57,7 @@ public class Populate implements CommandLineRunner {
             produto.setImagen("Fritas");
             produtos.add(produto);
 
+            produto = new ProdutoDTO();
             produto.setNomeProduto("Refrigerante");
             produto.setDescricaoProduto("Coca-cola, pepsi, fanta laranja, guaraná anterctica");
             produto.setCategoria(CategoriaProduto.BEBIDA);
@@ -55,6 +65,7 @@ public class Populate implements CommandLineRunner {
             produto.setImagen("Bebida");
             produtos.add(produto);
 
+            produto = new ProdutoDTO();
             produto.setNomeProduto("Pudim");
             produto.setDescricaoProduto("Pudim de leite condensado");
             produto.setCategoria(CategoriaProduto.SOBREMESA);
@@ -63,19 +74,21 @@ public class Populate implements CommandLineRunner {
             produtos.add(produto);
 
             List<ProdutoDTO> allProdutos = produtoRepository.findAll();
-            if (allProdutos.isEmpty() || allProdutos.size() == 0) {
+            if (allProdutos.isEmpty()) {
                 for (ProdutoDTO prod : produtos) {
                     produtoRepository.create(prod);
                 }
+                return true;
             }
 
         } catch (Exception err) {
             System.out.println("erro ao criar produtos");
             System.out.println(err);
         }
+        return false;
     }
 
-    public void insertClientes() {
+    public boolean insertClientes() {
         try {
 
             ArrayList<ClienteDTO> clientes = new ArrayList<>();
@@ -86,27 +99,88 @@ public class Populate implements CommandLineRunner {
             cliente.setEmail("joseSilva@gmail.com");
             clientes.add(cliente);
 
+            cliente = new ClienteDTO();
             cliente.setNomeCliente("Fernando de Sousa");
             cliente.setCpf("656.123.789-71");
             cliente.setEmail("fernadoSousa@gmail.com");
             clientes.add(cliente);
 
+            cliente = new ClienteDTO();
+            cliente.setNomeCliente("Bianca Brenda Lara da Mata");
+            cliente.setCpf("635.392.007-54");
+            cliente.setEmail("bianca_damata@brws.com.br");
+            clientes.add(cliente);
+
+            cliente = new ClienteDTO();
+            cliente.setNomeCliente("Nelson Bryan Diego Rodrigues");
+            cliente.setCpf("189.167.254-10");
+            cliente.setEmail("nelson_rodrigues@live.com.pt");
+            clientes.add(cliente);
+
+            cliente = new ClienteDTO();
+            cliente.setNomeCliente("Carolina Rayssa Allana Pereira");
+            cliente.setCpf("478.692.757-00");
+            cliente.setEmail("carolina.rayssa.pereira@somma.net.br");
+            clientes.add(cliente);
+
             List<ClienteDTO> allClientes = clienteRepository.findAll();
-            if (allClientes.isEmpty() || allClientes.size() == 0) {
+            if (allClientes.isEmpty()) {
                 for (ClienteDTO clie : clientes) {
                     clienteRepository.create(clie);
+                }
+                return true;
+            }
+
+        } catch (Exception err) {
+            System.out.println("erro ao criar Clientes");
+            System.out.println(err);
+        }
+        return false;
+    }
+
+    public void createPedidos() {
+        try {
+
+            ArrayList<PedidoDTO> pedidos = new ArrayList<>();
+
+            var pedido = new PedidoDTO();
+            pedido.setCliente(1L);
+            pedido.setListaProduto(Arrays.asList(1L, 3L, 4L));
+            pedido.setObservacao("Sem Bacon");
+            pedidos.add(pedido);
+
+            pedido = new PedidoDTO();
+            pedido.setListaProduto(Arrays.asList(2L, 3L));
+            pedido.setObservacao("Sem Queijo");
+            pedidos.add(pedido);
+
+            pedido = new PedidoDTO();
+            pedido.setCliente(2L);
+            pedido.setListaProduto(List.of(5L));
+            pedidos.add(pedido);
+
+            pedido = new PedidoDTO();
+            pedido.setCliente(3L);
+            pedido.setListaProduto(Arrays.asList(1L, 1L, 4L));
+            pedido.setObservacao("Sem Maionese");
+            pedidos.add(pedido);
+
+
+            List<PedidoOutDTO> allPedidos = pedidoServicePort.findAll();
+            if (allPedidos.isEmpty()) {
+                for (PedidoDTO pdi : pedidos) {
+                    pedidoServicePort.create(pdi);
                 }
             }
 
         } catch (Exception err) {
-            System.out.println("erro ao criar produtos");
+            System.out.println("erro ao criar Pedidos");
             System.out.println(err);
         }
     }
 
     @Override
     public void run(String... args) throws Exception {
-        insertProdutos();
-        insertClientes();
+        if (insertProdutos() && insertClientes()) createPedidos();
     }
 }
